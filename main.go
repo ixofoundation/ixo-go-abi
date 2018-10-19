8d1cd36ab54e5b294d1437123dd884c9067f0be9
Nicolaas0411 <nicolaas.vercuiel73@gmail.com>
2018-10-19 15:48:28 +0200

    # Added project wallet creation example. # Added wallet of example.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/hex"
 	"fmt"
 	"log"
 
@@ -9,16 +10,30 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
+	project "github.com/ixofoundation/ixo-go-abi/abi/project"
 	token "github.com/ixofoundation/ixo-go-abi/abi/token"
 )
 
 func main() {
-	key, _ := crypto.GenerateKey()
-	authorizer := bind.NewKeyedTransactor(key)
+
+	// My Robsten Account
+	privateKey, err := crypto.HexToECDSA("D55B3040D0442BBBDA8593C847E2BF4561CB942C63931609BE0992B7FCB19673")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	myRopstenAccount := bind.NewKeyedTransactor(privateKey)
+
 	callOpts := bind.CallOpts{
 		Pending: false,
-		From:    authorizer.From,
+		From:    myRopstenAccount.From,
 		Context: context.Background(),
+	}
+
+	transOpts := bind.TransactOpts{
+		From:     myRopstenAccount.From,
+		Signer:   myRopstenAccount.Signer,
+		GasLimit: uint64(5000000),
 	}
 
 	url := "https://ropsten.infura.io/sq19XM5Eu2ANGAzwZ4yk"
@@ -30,22 +45,43 @@ func main() {
 
 	fmt.Println("we have a connection")
 
+	// Test Ropsten balance
 	account := common.HexToAddress("0x647CD1829Ad0FF896640FCd3a29cF6Af0dE10A83")
 	balance, err := client.BalanceAt(context.Background(), account, nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("Address balance", balance)
 
-	fmt.Println(balance)
-
+	// ERC20
 	tokenAddress := common.HexToAddress("0x827a41c26784e0f51081e6d26687bff9c1c667e6")
-
 	ixoTokenContract, ixoError := token.NewIxoERC20Token(tokenAddress, client)
 
 	if ixoError != nil {
 		log.Fatal(ixoError)
 	}
-
 	cap, _ := ixoTokenContract.CAP(&callOpts)
 	fmt.Println("IXO Token Cap:", cap)
+
+	// Project
+	hexEncodedProjectDid := hex.EncodeToString([]byte("G8pj1V1Bcco7NpoGfkqY7K"))
+	var projectDid [32]byte
+	copy(projectDid[:], []byte("0x"+hexEncodedProjectDid))
+
+	projectRegAddress := common.HexToAddress("0xfe45b990a1dd890adfac13b0a9c77758cc83a862")
+	projectRegContract, projectError := project.NewProjectWalletRegistry(projectRegAddress, client)
+
+	if projectError != nil {
+		log.Fatal(projectError)
+	}
+
+	fmt.Println("ProjectDid: ", string(projectDid[:]));
+
+	transaction, _ := projectRegContract.EnsureWallet(&transOpts, projectDid)
+
+	client.SendTransaction(context.Background(), transaction)
+
+	walletAddress, _ := projectRegContract.WalletOf(&callOpts, projectDid)
+
+	fmt.Println("Wallet address: ", walletAddress.Hex());
 }
